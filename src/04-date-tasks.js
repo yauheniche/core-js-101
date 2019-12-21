b38ae22bd1d738e9479055e1e0674d3c535d7a4a
yauheniche <cherkasyauheni@gmail.com>
2019-12-22 02:44:31 +0300

    add date

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,9 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  const now = new Date(value);
+  return now;
 }
 
 /**
@@ -34,8 +35,9 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  const d = new Date(value);
+  return d;
 }
 
 
@@ -59,22 +61,22 @@ function isLeapYear(/* date */) {
 
 
 /**
- * Returns the string represention of the timespan between two dates.
- * The format of output string is "HH:mm:ss.sss"
- *
- * @param {date} startDate
- * @param {date} endDate
- * @return {string}
- *
- * @example:
- *    Date(2000,1,1,10,0,0),  Date(2000,1,1,11,0,0)   => "01:00:00.000"
- *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,30,0)       => "00:30:00.000"
- *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,20)        => "00:00:20.000"
- *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
- *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
- */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+* Returns the string represention of the timespan between two dates.
+* The format of output string is "HH:mm:ss.sss"
+*
+* @param {date} startDate
+* @param {date} endDate
+* @return {string}
+*
+* @example:
+*    Date(2000,1,1,10,0,0),  Date(2000,1,1,11,0,0)   => "01:00:00.000"
+*    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,30,0)       => "00:30:00.000"
+*    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,20)        => "00:00:20.000"
+*    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
+*    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
+*/
+function timeSpanToString(startDate, endDate) {
+  return new Date(endDate - startDate).toISOString().slice(11, -1);
 }
 
 
@@ -83,15 +85,19 @@ function timeSpanToString(/* startDate, endDate */) {
  * for the specified Greenwich time.
  * If you have problem with solution please read: https://en.wikipedia.org/wiki/Clock_angle_problem
  *
- * @param {date} date
- * @return {number}
  *
- * @example:
- *    Date.UTC(2016,2,5, 0, 0) => 0
- *    Date.UTC(2016,3,5, 3, 0) => Math.PI/2
- *    Date.UTC(2016,3,5,18, 0) => Math.PI
- *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
- */
+*
+ *
+*
+* @param {date} date
+* @return {number}
+*
+* @example:
+*    Date.UTC(2016,2,5, 0, 0) => 0
+*    Date.UTC(2016,3,5, 3, 0) => Math.PI/2
+*    Date.UTC(2016,3,5,18, 0) => Math.PI
+*    Date.UTC(2016,3,5,21, 0) => Math.PI/2
+*/
 function angleBetweenClockHands(/* date */) {
   throw new Error('Not implemented');
 }
